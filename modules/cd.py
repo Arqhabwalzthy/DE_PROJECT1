a2ec9f241f165e7d5b3e179b1569e3f87c4e097f
ArqhabwalzthyA13 <103428276+Arqhabwalzthy@users.noreply.github.com>
2023-09-15 21:23:25 +0800

    Delete modules directory

--- modules/cd.py
+++
@@ -1,6 +0,0 @@
-from modules.library_item import Libraryitem
-
-class Cd(Libraryitem):
-    def __init__(self, title, upc, subject, artist):
-        super(). __init__(title, upc, subject)
-        self.artist = artist 
--- modules/dvd.py
+++
@@ -1,9 +0,0 @@
-from modules.library_item import Libraryitem
-
-
-class Dvd(Libraryitem):
-    def __init__(self, title, upc, subject, actors, directors, genre):
-        super().__init__(title, upc, subject)
-        self.actors = actors
-        self.directors = directors
-        self.genre = genre
